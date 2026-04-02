45c63f83f9463e26c03c76160364cfc5c6ba516a
Rishu Kumar <rishu_kumar@live.com>
2026-04-02 10:30:25 +0530

    remove cluster pin from metacentrum scripts

--- run_mt_metacentrum.sh
+++ run_mt_metacentrum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q gpu@pbs-m1.metacentrum.cz
 #PBS -N train-mt
-#PBS -l select=1:cluster=bee*|zia*:ncpus=4:ngpus=1:mem=48000mb:gpu_mem=24000mb:scratch_local=100gb
+#PBS -l select=1:ncpus=4:ngpus=1:mem=48000mb:gpu_mem=24000mb:scratch_local=100gb
 #PBS -l walltime=48:00:00
 
 PROJECT_DIR="${PROJECT_DIR:-$HOME/IWSLT2026-mt}"
